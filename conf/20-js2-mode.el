ae34ef171f8cf46941ffa7112c84a12acbd5afaa
Ikeda Masaya <masaya.ikeda0914@outlook.jp>
2020-09-30 22:44:28 +0900

    add typescript environment

--- conf/20-js2-mode.el
+++ conf/20-js2-mode.el
@@ -1,7 +1,7 @@
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-jsx-mode))
-(add-to-list 'auto-mode-alist '("\\.json\\'" . js2-mode))
+;(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-jsx-mode))
+;(add-to-list 'auto-mode-alist '("\\.json\\'" . js2-mode))
 
-(add-hook 'js2-mode-hook
-          (lambda ()
-            (setq js2-basic-offset 2)
-            (setq js-switch-indent-offset 2)))
+;(add-hook 'js2-mode-hook
+;          (lambda ()
+;            (setq js2-basic-offset 2)
+;            (setq js-switch-indent-offset 2)))
