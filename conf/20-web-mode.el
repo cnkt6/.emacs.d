58fa970cd1d0ca3e45aee618399a4be86ff7cff6
Ikeda Masaya <masaya.ikeda0914@outlook.jp>
2020-10-09 13:01:54 +0900

    fix: fixed typescript mode conf

--- conf/20-web-mode.el
+++ conf/20-web-mode.el
@@ -1,11 +1,17 @@
-(add-to-list 'auto-mode-alist '("\\.[jt]sx\\'" . web-mode))
-(defun custom-web-mode-hook()
-  (setq web-mode-attr-indent-offset nil)
-  (setq web-mode-marup-indent-offset 4)
-  (setq web-mode-css-indent-offset 4)
-  (setq indent-tabs-mode nil)
-  (setq tab-width 4))
+;;(add-to-list 'auto-mode-alist '("\\.[jt]sx\\'" . web-mode))
+;; 
+;;(defun custom-web-mode-hook()
+;;  (setq web-mode-attr-indent-offset nil)
+;;  (setq web-mode-marup-indent-offset 4)
+;;  (setq web-mode-css-indent-offset 4)
+;;(setq indent-tabs-mode nil)
+;;(setq tab-width 4))
 
-(flycheck-add-mode 'javascript-eslint 'web-mode)
-(use-package add-node-modules-path
-             :hook (js-mode js2-mode web-mode))
+;;;;(flycheck-add-mode 'javascript-eslint 'web-mode)
+;;(use-package add-node-modules-path
+;;             :hook (js-mode js2-mode web-mode))
+;; 
+;;(add-hook 'web-mode-hook
+;;          (lambda ()
+;;            (eldoc-mode +1)))
+;;            
